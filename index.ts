4d58b710a1a5ef7424da4bc26efc9132efc82484
Dolores <alyciahathaways@gmail.com>
2020-08-03 10:44:37 +0800

    处理错误

--- index.ts
+++ index.ts
@@ -10,29 +10,28 @@ const publicDir = p.resolve(__dirname, 'public')
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
     const {method, url: path, headers} = request
     const {pathname, search} = url.parse(path)
-    switch (pathname) {
-        case('/index.html'):
-            response.setHeader('Contend-Type', 'text/html; charset=utf-8')
-            fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
-                if (error) throw error
-                response.end(data.toString())
-            })
-            break
-        case('/style.css'):
-            response.setHeader('Contend-Type', 'text/css; charset=utf-8')
-            fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
-                if (error) throw error
-                response.end(data.toString())
-            })
-            break
-        case('/main.js'):
-            response.setHeader('Contend-Type', 'text/javascript; charset=utf-8')
-            fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
-                if (error) throw error
-                response.end(data.toString())
-            })
-            break
+    let filename = pathname.slice(1)
+    if (filename === '') {
+        filename = 'index.html'
     }
+
+    // response.setHeader('Contend-Type', 'text/html; charset=utf-8')
+    fs.readFile(p.resolve(publicDir, filename), (error, data) => {
+        if (error) {
+            if (error.errno === -2) {
+                response.statusCode = 404
+                fs.readFile(p.resolve(publicDir, '404.html'), (error, data) => {
+                    response.end(data)
+                })
+            } else {
+                response.statusCode = 500
+                response.end('服务器内部错误')
+            }
+        } else {
+            response.end(data)
+        }
+    })
+
 
 })
 
